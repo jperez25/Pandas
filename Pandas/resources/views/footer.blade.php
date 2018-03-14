4d6440e7e3a326b22a680ff343ec4bdb3c61a4f9
jperez25 <jova9991@gmail.com>
2018-03-13 22:12:18 -0500

    More chages to index, navbar, footer

--- Pandas/resources/views/footer.blade.php
+++ Pandas/resources/views/footer.blade.php
@@ -1,94 +1,13 @@
 <!--Footer-->
 <footer class="page-footer font-small stylish-color-dark pt-4 mt-4">
 
-    <!--Footer Links-->
-    <div class="container text-center text-md-left">
-        <div class="row">
-
-            <!--First column-->
-            <div class="col-md-4">
-                <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Footer Content</h5>
-                <p>Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit
-                    amet, consectetur adipisicing elit.</p>
-            </div>
-            <!--/.First column-->
-
-            <hr class="clearfix w-100 d-md-none">
-
-            <!--Second column-->
-            <div class="col-md-2 mx-auto">
-                <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Links</h5>
-                <ul class="list-unstyled">
-                    <li>
-                        <a href="#!">Link 1</a>
-                    </li>
-                    <li>
-                        <a href="#!">Link 2</a>
-                    </li>
-                    <li>
-                        <a href="#!">Link 3</a>
-                    </li>
-                    <li>
-                        <a href="#!">Link 4</a>
-                    </li>
-                </ul>
-            </div>
-            <!--/.Second column-->
-
-            <hr class="clearfix w-100 d-md-none">
-
-            <!--Third column-->
-            <div class="col-md-2 mx-auto">
-                <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Links</h5>
-                <ul class="list-unstyled">
-                    <li>
-                        <a href="#!">Link 1</a>
-                    </li>
-                    <li>
-                        <a href="#!">Link 2</a>
-                    </li>
-                    <li>
-                        <a href="#!">Link 3</a>
-                    </li>
-                    <li>
-                        <a href="#!">Link 4</a>
-                    </li>
-                </ul>
-            </div>
-            <!--/.Third column-->
-
-            <hr class="clearfix w-100 d-md-none">
-
-            <!--Fourth column-->
-            <div class="col-md-2 mx-auto">
-                <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Links</h5>
-                <ul class="list-unstyled">
-                    <li>
-                        <a href="#!">Link 1</a>
-                    </li>
-                    <li>
-                        <a href="#!">Link 2</a>
-                    </li>
-                    <li>
-                        <a href="#!">Link 3</a>
-                    </li>
-                    <li>
-                        <a href="#!">Link 4</a>
-                    </li>
-                </ul>
-            </div>
-            <!--/.Fourth column-->
-        </div>
-    </div>
-    <!--/.Footer Links-->
-
     <hr>
 
     <!--Call to action-->
     <div class="text-center py-3">
         <ul class="list-unstyled list-inline mb-0">
             <li class="list-inline-item">
-                <h5 class="mb-1">Order online</h5>
+                <h5 class="mb-1">Order Now!</h5>
             </li>
             <li class="list-inline-item">
                 <a href="#!" class="btn btn-danger btn-rounded">Sign up!</a>
@@ -133,7 +52,7 @@
 
     <!--Copyright-->
     <div class="footer-copyright py-3 text-center">
-        © 2018 Copyright: 2016 www.luckypandaonline.com  All rights reserved.
+        © 2018 Copyright: www.luckypandaonline.com  All rights reserved.
         <br/>
         Site by
         <a href="https://github.com/jperez25/Pandas"> Pandas project </a>
--- Pandas/resources/views/index.blade.php
+++ Pandas/resources/views/index.blade.php
@@ -11,6 +11,8 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <!--Bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+        <!--favicon 
+        <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.png" /> -->
 
         <style>
             body{background-color: #fefff2}
@@ -25,11 +27,17 @@
         <div class="container">
             <!-- Main component for a primary marketing message or call to action -->
             <div class="jumbotron">
-                <h1>Navbar example</h1>
-                <img src="../images/banner.jpg" alt="banner" height="42" width="42">
+                <!-- to use resources such as images, files should be in public folder -->
+                <img src="images/banner.jpg" alt="banner" width="100%"></img>
             </div>
 
             <!-- Some images/links to menu, order online and sign in -->
+            <div class="row">
+                    <span class="col-md-2"></span>
+                    <img class="col-md-3" alt="x">
+                    <img class="col-md-3" alt="y">
+                    <img class="col-md-3" alt="z">
+            </div>
 
         </div>
 
